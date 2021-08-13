ceb9b102aa6a7a392007504887d8a60dc8b4e51f
diekus <73939538+diekus@users.noreply.github.com>
2021-08-13 09:15:05 +0100

    adds theme support

--- scripts/pwinter.js
+++ scripts/pwinter.js
@@ -1,7 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
+    init();
+    
+});
+
+let init = () => {
+    //sets events on color pickers
     document.querySelectorAll('input[type="color"]').forEach(picker => {
         picker.addEventListener('input', function() {
-            document.documentElement.style.setProperty("--" + this.name, this.value);
+            document.querySelector(':root').style.setProperty("--" + this.name, this.value);
         }, false);
     });
-});
+
+    document.querySelectorAll('.bgBtn').forEach(themeBgBtn => {
+        themeBgBtn.addEventListener('click', function() {
+            setBackgroundTheme(this.id);
+        }, false);
+    });
+};
+
+let setBackgroundTheme = (theme) => {
+    switch(theme) {
+        case 'btnDark': 
+            document.getElementById('canvasPreview').style.backgroundColor = 'var(--colorDark)';
+            break;
+        case 'btnLight': 
+            document.getElementById('canvasPreview').style.backgroundColor = 'var(--colorLight)';
+            break;
+    }
+};
