96dbe07d179088499b6847b7e178d9834feee278
diekus <diekus@gmail.com>
2021-08-13 14:47:08 +0100

    Saves the pwa logo
    adds file system access to save the custom svg file

--- scripts/pwinter.js
+++ scripts/pwinter.js
@@ -11,11 +11,15 @@ let init = () => {
         }, false);
     });
 
+    //sets the backgroundToggle events
     document.querySelectorAll('.bgBtn').forEach(themeBgBtn => {
         themeBgBtn.addEventListener('click', function() {
             setBackgroundTheme(this.id);
         }, false);
     });
+
+    //
+    document.getElementById('btnSave').addEventListener('click', saveLogo, false);
 };
 
 let setBackgroundTheme = (theme) => {
@@ -28,3 +32,58 @@ let setBackgroundTheme = (theme) => {
             break;
     }
 };
+
+async function saveLogo () {
+    //prepares the logo
+    let s = window.getComputedStyle(document.querySelector(':root'));
+    let content = document.getElementById('pwaLogo').innerHTML;
+    content = content.replace('var(--colorP)', s.getPropertyValue('--colorP'));
+    content = content.replace('var(--colorW)', s.getPropertyValue('--colorW'));
+    content = content.replace('var(--colorA)', s.getPropertyValue('--colorA'));
+    
+    //prepares the file
+    let fileHandle;
+    try {
+        fileHandle = await getNewFileHandle();
+    } catch (ex) {
+        if (ex.name === 'AbortError') {
+            return;
+        }
+        const msg = 'An error occured trying to open the file.';
+        console.error(msg, ex);
+        alert(msg);
+        return;
+    }
+    try {
+        await writeFile(fileHandle, content);
+    }
+    catch (ex) {
+        const msg = 'Unable to save file.';
+        console.error(msg, ex);
+        alert(msg);
+        return;
+    }
+}
+
+async function getNewFileHandle() {
+    const options = {
+        startIn: 'pictures',
+        suggestedName: 'custom PWA logo.svg',
+        types: [
+            {
+                description: 'SVG Files',
+                accept: {
+                    'image/svg+xml': ['.svg'],
+                },
+            },
+        ],
+    };
+    const handle = await window.showSaveFilePicker(options);
+    return handle;
+}
+
+async function writeFile (fileHandle, contents) {
+    const writable = await fileHandle.createWritable();
+    writable.write(contents);
+    writable.close();
+}
